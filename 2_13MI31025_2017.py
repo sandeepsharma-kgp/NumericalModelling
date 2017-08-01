3fe4a895b9428724d487c49b6c9c78ec5c0a3367
peace <sandeep.sharma@happay.in>
2017-08-01 19:28:15 +0530

    lab2 code completed

--- 2_13MI31025_2017.py
+++ 2_13MI31025_2017.py
@@ -1,21 +1,23 @@
 import numpy as np
 if __name__ == "__main__":
 	n = int(raw_input("Enter size of matrix: "))
-	a = [[0 for i in range(n)] for j in range(n)]
-	x = [1 for j in range(n)]
-	e = [0 for j in range(n)]
+	a = [[0. for i in range(n)] for j in range(n)]
+	x = [1. for j in range(n)]
+	e = [0. for j in range(n)]
 	for i in range(n):
 		for j in range(n):
 			a[i][j] = int(raw_input("Value of a[{}][{}]: ".format(i,j)))
 	a = np.matrix(a)
 	x = np.matrix(x).T
-	emax = 0.0
-	while (emax>10e-2):
-		z = [0 for j in range(n)]
+	emax = 1.0
+	while (emax>10e-4):
+		z = [0. for j in range(n)]
+		z = np.matrix(z).T
 		z += a*x
 		zmax = np.matrix.max(np.fabs(z))
 		z = z/zmax
-		e = z - x
+		e = np.fabs(z - x)
 		emax = np.matrix.max(e)
 		x = z
-	print emax
+	print "\nEigen Value: ", zmax
+	print "\nEigen Vector: \n", z
