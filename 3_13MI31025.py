adf7987eff9aea267b7ab28a3a4e47a5494877ce
peace <sandeep.sharma@happay.in>
2017-08-16 13:13:21 +0530

    Lab3 modified

--- 3_13MI31025.py
+++ 3_13MI31025.py
@@ -11,21 +11,21 @@ log_Sl = np.log(Sl)
 def func(params, xdata, ydata):
     return (ydata - np.dot(xdata, params))
 x0 = np.array([0.0, 0.0])
-xdata = np.transpose(np.array([[1.0, 1.0, 1.0, 1.],
+xdata = np.transpose(np.array([[1.0, 1.0, 1.0, 1.0],
                                log_l]))
 ydata = log_Sl
 log_k, b = optimization.leastsq(func, x0, args=(xdata, ydata))[0]
 S = np.exp(log_k + b * np.log(1000)) * 1e6
 
 
-def fwp(wp, wg, D, derivative=False):
-    gama = 2600
-    F = 1.35
+def fwp(wp, wg, D, F, derivative=False):
+    roh = 2600
     h = 3.
+    g = 9.8
     a = - 0.36 * S / h
-    b = F * (gama * D) - S * .64
-    c = 2 * wg * F * (gama * D)
-    d = (gama * D) * F * wg**2
+    b = F * (roh * g * D) - S * .64
+    c = 2 * wg * F * (roh * g * D)
+    d = (roh * g * D) * F * wg**2
     if not derivative:
         return (a * wp**3 + b * wp**2 + c * wp + d)
     else:
@@ -33,10 +33,20 @@ def fwp(wp, wg, D, derivative=False):
 
 wp1 = 1000
 while True:
-    wp2 = wp1 - fwp(wp1, 4.5, 200.) / fwp(wp1, 4.5, 200., True)
+    wp2 = wp1 - fwp(wp1, 4.5, 1.35, 200.) / fwp(wp1, 4.5, 200., 1.35, True)
     if abs(wp1 - wp2) < .00001:
         break
     else:
         wp1 = wp2
 
+print S / 1e6
 print wp1
+
+
+table = np.matrix([12., 15., 18., 19.5],
+                  [13.5, 16.5, 19.5, 21.],
+                  [16.5., 19.5, 22.5, 25.5],
+                  [22.5, 25.5, 30., 34.5],
+                  [28.5, 34.5, 39., 45.],
+                  [39., 42., 45., 48.])
+
